fed90c5241d6f9e7177c73de5cf50b568bf94ef9
Thami A. Vicente <thamiavicente@gmail.com>
2021-08-05 17:08:27 -0300

    Fixing Eslint bugs from useStore and store

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -1,10 +1,8 @@
 import { readonly } from 'vue'
 import UserModule from './user'
-import GlobalModule from './user'
+import GlobalModule from './global'
 
-export default readonly ({
-    User: UserModule,
-    Global: GlobalModule
+export default readonly({
+  User: UserModule,
+  Global: GlobalModule
 })
-
-  
