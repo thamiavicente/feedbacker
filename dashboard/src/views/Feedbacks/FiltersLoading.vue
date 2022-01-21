4239c775c4dd02be4ead91f90e6fb5beadd557b8
Thamiris Vicente <thamiavicente@gmail.com>
2022-01-20 22:52:02 -0300

    Adding padding to filter loading

--- dashboard/src/views/Feedbacks/FiltersLoading.vue
+++ dashboard/src/views/Feedbacks/FiltersLoading.vue
@@ -1,36 +1,36 @@
 <template>
   <content-loader
-    class="flex flex-col rounded"
+    class="flex flex-col rounded px-4"
     width="100%"
     height="300px"
   >
-  <content-loader
-    class="mt-3 rounded"
-    width="90%"
-    height="28px"
-    animation-duration="2s"
-  />
+    <content-loader
+      class="mt-3 rounded"
+      width="90%"
+      height="28px"
+      animation-duration="2s"
+    />
 
-  <content-loader
-    class="mt-3 rounded"
-    width="60%"
-    height="28px"
-    animation-duration="2.3s"
-  />
+    <content-loader
+      class="mt-3 rounded"
+      width="60%"
+      height="28px"
+      animation-duration="2.3s"
+    />
 
-  <content-loader
-    class="mt-3 rounded"
-    width="80%"
-    height="28px"
-    animation-duration="2.7s"
-  />
+    <content-loader
+      class="mt-3 rounded"
+      width="80%"
+      height="28px"
+      animation-duration="2.7s"
+    />
 
-  <content-loader
-    class="mt-3 rounded"
-    width="70%"
-    height="28px"
-    animation-duration="2.9s"
-  />
+    <content-loader
+      class="mt-3 rounded"
+      width="70%"
+      height="28px"
+      animation-duration="2.9s"
+    />
   </content-loader>
 </template>
 
