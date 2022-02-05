b05244a78267fabd6f4257da78e218a484d6f747
Thamiris Vicente <thamiavicente@gmail.com>
2022-02-04 22:16:35 -0300

    Create home tests e2e

---
+++ dashboard/tests/e2e/specs/home.spec.js
@@ -0,0 +1,83 @@
+const APP_URL = process.env.APP_URL || 'http://localhost:8080'
+
+describe ('Home', () => {
+    it ('should render create account modal when click on cta create account button', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#cta-create-account-button').click()
+
+        cy.get('#modal-create-account')
+    })
+
+    it ('should render create account modal when click on header create account button', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#header-create-account-button').click()
+
+        cy.get('#modal-create-account')
+    })
+
+    it ('should render login modal when click on header login button', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#header-login-button').click()
+
+        cy.get('#modal-login')
+    })
+
+    it ('should login when email and password correct', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#header-login-button').click()
+        cy.get('#modal-login')
+
+        cy.get('#email-field').type('thami@email.com')
+        cy.get('#password-field').type('1234')
+        cy.get('#submit-button').click()
+
+        cy.url().should('include', '/feedbacks')
+    })
+
+    it ('should return error with invalid email', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#header-login-button').click()
+        cy.get('#modal-login')
+
+        cy.get('#email-field').type('thami@')
+        cy.get('#password-field').type('1234')
+        cy.get('#submit-button').click()
+
+        cy.get('#email-error')
+    })
+
+    it ('should return error with invalid password', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#header-login-button').click()
+        cy.get('#modal-login')
+
+        cy.get('#email-field').type('thami@email.com')
+        cy.get('#password-field').type('12')
+        cy.get('#submit-button').click()
+
+        cy.get('#password-error')
+    })
+
+    it ('should logout when click on logout button', () => {
+        cy.visit(APP_URL)
+
+        cy.get('#header-login-button').click()
+        cy.get('#modal-login')
+
+        cy.get('#email-field').type('thami@email.com')
+        cy.get('#password-field').type('1234')
+        cy.get('#submit-button').click()
+
+        cy.url().should('include', '/feedbacks')
+        cy.wait(2000)
+
+        cy.get('#logout-button').click()
+        cy.url().should('include', '/')
+    })
+})
